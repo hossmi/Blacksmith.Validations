bc93adf10e1df2144264060d29c6afe9f3c420c0
hossmi <hossmi@gmail.com>
2020-02-09 20:24:39 +0100

    rename private method

--- Blacksmith.Validations/Asserts.cs
+++ Blacksmith.Validations/Asserts.cs
@@ -24,42 +24,42 @@ namespace Blacksmith.Validations
 
         public void isFalse(bool condition)
         {
-            validate(condition == false, () => new FalseExpectedAssertException());
+            prv_validate(condition == false, () => new FalseExpectedAssertException());
         }
 
         public void isTrue(bool condition)
         {
-            validate(condition, () => new TrueExpectedAssertException());
+            prv_validate(condition, () => new TrueExpectedAssertException());
         }
 
         public void isInstanceOf<T>(object item)
         {
-            validate(item != null, () => new NotNullExpectedObjectAssertException());
-            validate(item is T, () => new ExpectedTypeAssertException(item.GetType(), typeof(T)));
+            prv_validate(item != null, () => new NotNullExpectedObjectAssertException());
+            prv_validate(item is T, () => new ExpectedTypeAssertException(item.GetType(), typeof(T)));
         }
 
         public void isNotNull<T>(T item) where T : class
         {
-            validate(item != null, () => new NotNullExpectedObjectAssertException());
+            prv_validate(item != null, () => new NotNullExpectedObjectAssertException());
         }
 
         public void isNull<T>(T item) where T : class
         {
-            validate(item == null, () => new NullExpectedObjectAssertException());
+            prv_validate(item == null, () => new NullExpectedObjectAssertException());
         }
 
         public void isValidEnum<T>(T enumValue) where T : struct
         {
-            validate(Enum.IsDefined(typeof(T), enumValue), () => new ValidEnumValueExpectedAssertException(typeof(T)));
+            prv_validate(Enum.IsDefined(typeof(T), enumValue), () => new ValidEnumValueExpectedAssertException(typeof(T)));
         }
 
         public void isValidEnumeration<T>(T value) where T : Enumeration
         {
-            validate(value != null, () => new NotNullExpectedObjectAssertException());
-            validate(Enumeration.isDefined(value), () => new ValidEnumerationObjectExpectedAssertException(enumerationType: typeof(T)));
+            prv_validate(value != null, () => new NotNullExpectedObjectAssertException());
+            prv_validate(Enumeration.isDefined(value), () => new ValidEnumerationObjectExpectedAssertException(enumerationType: typeof(T)));
         }
 
-        private static void validate(bool condition, Func<AssertException> buildException)  
+        private static void prv_validate(bool condition, Func<AssertException> buildException)  
         {
             if (condition == false)
             {
