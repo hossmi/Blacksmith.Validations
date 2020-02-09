dd99d6f7f74da946ee851a8cf9f1aa948b2b260b
hossmi <hossmi@gmail.com>
2020-02-09 20:20:52 +0100

    add new assert exception type

---
+++ Blacksmith.Validations/Exceptions/NullOrEmptyStringAssertException.cs
@@ -0,0 +1,23 @@
+﻿using Blacksmith.Validations.Exceptions;
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace Blacksmith.Validations.Exceptions
+{
+    [Serializable]
+    public class NullOrEmptyStringAssertException : AssertException
+    {
+        public NullOrEmptyStringAssertException(
+            [CallerLineNumber] int callerLineNumber = 0
+            , [CallerMemberName] string callerMemberName = ""
+            , [CallerFilePath] string callerFilePath = "") 
+            : base(callerLineNumber, callerMemberName, callerFilePath)
+        {
+        }
+
+        protected NullOrEmptyStringAssertException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
